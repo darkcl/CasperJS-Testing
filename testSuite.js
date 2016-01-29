43dcb7507bdf5cbae570f4267cdf1ba83595800c
Yeung Yiu Hung <yeungyiuhung@cherrypicks.com>
2016-01-29 10:48:25 +0800

    123

--- testSuite.js
+++ testSuite.js
@@ -1,28 +1,28 @@
-casper.test.begin('Google search retrieves 10 or more results', 5, function suite(test) {
-    casper.start("http://www.google.com/", function() {
-        test.assertTitle("Google", "google homepage title is the one expected");
-        test.assertExists('form[action="/search"]', "main form is found");
-        this.fill('form[action="/search"]', {
-            q: "Testing"
-        }, true);
-    });
+// casper.test.begin('Google search retrieves 10 or more results', 5, function suite(test) {
+//     casper.start("http://www.google.com/", function() {
+//         test.assertTitle("Google", "google homepage title is the one expected");
+//         test.assertExists('form[action="/search"]', "main form is found");
+//         this.fill('form[action="/search"]', {
+//             q: "Testing"
+//         }, true);
+//     });
 
-    casper.then(function() {
-        test.assertTitle("Testing - Google Search", "google title is ok");
-        test.assertUrlMatch(/q=Testing/, "search term has been submitted");
-        test.assertEval(function() {
-            return __utils__.findAll("h3.r").length >= 10;
-        }, "google search for \"Testing\" retrieves 10 or more results");
-        this.capture('foo.jpg', undefined, {
-            format: 'jpg',
-            quality: 75
-        });
-    });
+//     casper.then(function() {
+//         test.assertTitle("Testing - Google Search", "google title is ok");
+//         test.assertUrlMatch(/q=Testing/, "search term has been submitted");
+//         test.assertEval(function() {
+//             return __utils__.findAll("h3.r").length >= 10;
+//         }, "google search for \"Testing\" retrieves 10 or more results");
+//         this.capture('foo.jpg', undefined, {
+//             format: 'jpg',
+//             quality: 75
+//         });
+//     });
     
-    casper.run(function() {
-        test.done();
-    });
-});
+//     casper.run(function() {
+//         test.done();
+//     });
+// });
 
 casper.test.begin('Simple page content test', 3, function suite(test) {
     casper.start('./simple.html', function() {
